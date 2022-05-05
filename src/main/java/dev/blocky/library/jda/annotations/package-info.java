0de4e88f20a055ab0342d1d602613495afcd564e
Blocky.jar ツ <91844174+BlockyDotJar@users.noreply.github.com>
2022-05-05 17:46:07 +0200

    Fixed JavaDoc

--- src/main/java/dev/blocky/library/jda/annotations/package-info.java
+++ src/main/java/dev/blocky/library/jda/annotations/package-info.java
@@ -8,20 +8,24 @@
  * <br>
  *
  * <ul>
+ * <li>{@link dev.blocky.library.jda.annotations.Deadline Deadline} <br>
+ * This is an {@link java.lang.annotation.Annotation Annotation} which tells the developers that a method/class should
+ * be finish in the future.</li>
+ *
  * <li>{@link dev.blocky.library.jda.annotations.DeprecatedSince Deprecated Since} <br>
- * This is an Annotation which tells the developers since when a method/class is
+ * This is an {@link java.lang.annotation.Annotation Annotation} which tells the developers since when a method/class is
  * deprecated.</li>
  *
  * <li>{@link dev.blocky.library.jda.annotations.ForRemoval For Removal} <br>
- * This is an Annotation which tells the developers that a method/class should
+ * This is an {@link java.lang.annotation.Annotation Annotation} which tells the developers that a method/class should
  * be removed in the future.</li>
  *
  * <li>{@link dev.blocky.library.jda.annotations.ReplaceWith Replace With} <br>
- * This is an Annotation which tells the developers, with which method/class a
+ * This is an {@link java.lang.annotation.Annotation Annotation} which tells the developers with which method/class a
  * deprecated method/class should be replaced.</li>
  *
  * <li>{@link dev.blocky.library.jda.annotations.Undocumented Undocumented} <br>
- * This is an Annotation which tells the developers that a
+ * This is an {@link java.lang.annotation.Annotation Annotation} which tells the developers that a
  * method/class/constructor/package is not documented yet.</li>
  * </ul>
  */
